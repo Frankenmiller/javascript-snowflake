a201c473e7c79404d76ccaaa99d28fe1ade2054d
Frankenmiller <Dr.Frankenmiller@gmail.com>
2022-09-07 15:13:09 +0200

    Got pretty good snowfall effect and total circles capped at 500 to conserve power

--- js/sketch.js
+++ js/sketch.js
@@ -4,16 +4,28 @@ let gravity;
 function setup() {
     createCanvas(windowWidth, windowHeight);
     gravity = createVector(0, 0.01);
+    for (let i=0; i<500; i++) {
+        let x = random(width);
+        let y = random(height);
+        snow.push(new Snowflake(x, y));
+    }
 }
 
 function draw() {
     background(0);
-    snow.push(new Snowflake());
+    // snow.push(new Snowflake());
 
     for (flake of snow) {
+        flake.applyForce(gravity);
         flake.update();
         flake.render();
     }
+
+    // for (let i = snow.length-1; i>=0; i--) {
+    //     if (snow[i].offScreen()) {
+    //         snow.splice(i, 1);
+    //     }
+    // }
 }
 
 function windowResized() {
--- js/snowflake.js
+++ js/snowflake.js
@@ -1,22 +1,66 @@
+function getRandomSize() {
+
+    let r = randomGaussian() * 2.5;
+    return constrain(abs(r * r), 2, 36);
+
+    // while (true) {
+    //     let r1 = random(1);
+    //     let r2 = random(1);
+    //     if (r2 > r1) {
+    //         return r1 * 36;
+    //     }
+    // }
+}
+
 class Snowflake {
-    constructor() {
+    constructor(sx, sy) {
+        let x = sx || random(width);
+        let y = sy || random(-100, -10);
+        this.pos = createVector(x, y);
+        this.vel = createVector(0, 0.0);
+        this.acc = createVector();
+        this.r = getRandomSize();
+    }
+    
+    applyForce(force) {
+        let f = force.copy();
+        f.mult(this.r);
+        
+        // let f = force.copy();
+        // f.div(this.mass);
+        this.acc.add(f);
+    }
+    
+    randomize() {        
         let x = random(width);
         let y = random(-100, -10);
         this.pos = createVector(x, y);
-        this.vel = createVector(0, 0.5);
+        this.vel = createVector(0, 0.0);
         this.acc = createVector();
-        this.r = random(4, 8);
+        this.r = getRandomSize();
     }
 
     update() {
-        this.acc = gravity;
         this.vel.add(this.acc);
+        this.vel.limit(this.r * 0.2);
+        if (this.vel.mag() < 1) {
+            this.vel.normalize();
+        }
         this.pos.add(this.vel);
+        this.acc.mult(0);
+        
+        if (this.pos.y > height + this.r) {
+            this.randomize();
+        }
     }
 
     render() {
         stroke(255);
         strokeWeight(this.r);
         point(this.pos.x, this.pos.y);
+    }
+
+    offScreen() {
+        return (this.pos.y > height + this.r);
     }
 }
